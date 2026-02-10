550bb0a00d7b6640b881e2f0b928048a89b098b9
DEVISRI1235 <devisrinelakuditi125@gmail.com>
2026-02-10 14:00:30 +0530

    Update app.py

--- app.py/app.py
+++ app.py/app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+st.set_page_config(layout="wide")
+st.title("Enterprise Analytics Dashboard")
+
+session = get_active_session()
+
+df = session.sql(
+    "SELECT * FROM ENTERPRISE_DB.GOLD.VW_EXEC_SUMMARY"
+).to_pandas()
+
+st.dataframe(df)
 
